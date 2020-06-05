cf97a085b9250497e3e2f29f98724d6fbe17e380
= <=>
2020-06-04 17:17:27 -0700

    UPDATE TitleCaser.py

--- TitleCaser.py
+++ TitleCaser.py
@@ -1,30 +1,30 @@
 # According to ALA format, these words should not be capitalized if they are not the first or last words of the title.
-# The list of words to be left lowercase, given the right context, are stored in the array, noCap.
-noCap = ["and", "as", "but", "for", "nor", "or", "yet", "a", "an", "the", "at",
+# The list of words to be left lowercase, given the right context, are stored in the list, nocap.
+nocap = ["and", "as", "but", "for", "nor", "or", "yet", "a", "an", "the", "at",
          "in", "by", "of", "on", "to"]
 
 # A function tc (title case) takes in two parameters, x being the user's input, and the list of exceptions.
 def tc(x, excpt):
 
-    # x is modified into lowercase, then split at each space, resulting in an array of the lowercase words.
+    # x is modified into lowercase, then split at each space, resulting in a list of the lowercase words.
     x = x.lower().split(' ')
-    # The array new_arr is initialized with the requirement that the first word must be capitalized.
-    new_arr = [x[0].capitalize()]
+    # The list newlist is initialized with the requirement that the first word must be capitalized.
+    newlist = [x[0].capitalize()]
     # The for loop iterates from the second word and stops at, but does not include, the last word.
     for word in x[1:-1]:
-        # If the word is found in the excpt array...
+        # If the word is found in the excpt list...
         if word in excpt:
-            # The word is appended to new_arr as is (lowercase).
-            new_arr.append(word)
+            # The word is appended to newlist as is (lowercase).
+            newlist.append(word)
         else:
-            # Else if it is not found in excpt, the capitalized version is appended to new_arr.
-            new_arr.append(word.capitalize())
-    # Lastly, the final word is appended to new_arr, with the requirement that the last word must be capitalized.
-    new_arr.append(x[-1].capitalize())
-    # The function tc returns joined new_arr, with words separated by an empty space.
-    return ' '.join(new_arr)
+            # Else if it is not found in excpt, the capitalized version is appended to newlist.
+            newlist.append(word.capitalize())
+    # Lastly, the final word is appended to newlist, with the requirement that the last word must be capitalized.
+    newlist.append(x[-1].capitalize())
+    # The function tc returns joined newlist, with words separated by an empty space.
+    return ' '.join(newlist)
 
 # The user is prompted to enter their string of words.
-strInput = input("Enter the words you would like to title case:\n")
-# The result of function tc, with arguments strInput and noCap, are printed to the console.
-print(tc(strInput, noCap))
+strinput = input("Enter the words you would like to title case:\n")
+# The result of function tc, with arguments strinput and nocap, are printed to the console.
+print(tc(strinput, nocap))
